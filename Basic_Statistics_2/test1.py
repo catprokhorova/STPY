07d8071d76209c74f86ec4be1fc4852c2caf474e
Ekaterina Prokhorova <cat.prokhorova@gmail.com>
2023-04-26 21:39:54 +0300

    Init commit

---
+++ Basic_Statistics_2/test1.py
@@ -0,0 +1,5 @@
+import pytest
+from main1 import full_probability
+
+def test():
+    assert full_probability() == 0.958
---
+++ Basic_Statistics_2/test2.py
@@ -0,0 +1,5 @@
+import pytest
+from main2 import monte_carlo
+
+def test():
+    assert 0.7 < monte_carlo() < 0.9
